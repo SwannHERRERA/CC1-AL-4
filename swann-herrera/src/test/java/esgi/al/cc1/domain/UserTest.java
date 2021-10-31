9900b4b97e77cdbf4b0b1eced1ff38ba5124c666
Swann <swann@devloup.dev>
2021-10-31 11:16:54 +0100

    refacto test

--- swann-herrera/src/test/java/esgi/al/cc1/domain/UserTest.java
+++ swann-herrera/src/test/java/esgi/al/cc1/domain/UserTest.java
@@ -1,22 +1,22 @@
 package esgi.al.cc1.domain;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class UserTest {
   // TODO Create test for non lowercase email
   @Test
-  void testCreationOfUser() {
+  public void testCreationOfUser() {
     // Le UUID
     String firstName = "Swann";
     String lastName = "HERRERA";
     String email = "swann@devloup.dev";
     int age = 20;
     User user = User.of(firstName, lastName, email, age);
-    assertTrue(firstName.equals(user.getFirstName()));
-    assertTrue(lastName.equals(user.getLastName()));
-    assertTrue(email.equals(user.getEmail()));
-    assertTrue(age == user.getAge());
+    assertEquals(firstName, user.getFirstName());
+    assertEquals(lastName, user.getLastName());
+    assertEquals(email, user.getEmail());
+    assertEquals(age, user.getAge());
   }
 }
