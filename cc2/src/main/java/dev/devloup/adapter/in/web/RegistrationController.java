c878b592bf8788b6efc4f475e9891aadcd241625
Swann HERRERA <swann@devloup.dev>
2021-12-09 01:50:16 +0100

    path error in web apdater

--- cc2/src/main/java/dev/devloup/adapter/in/web/RegistrationController.java
+++ cc2/src/main/java/dev/devloup/adapter/in/web/RegistrationController.java
@@ -9,22 +9,22 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import dev.devloup.application.port.in.CreateUserCommand;
-import dev.devloup.application.service.UserService;
+import dev.devloup.application.port.in.CreateUserUseCase;
 
 @Path("/register")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class RegistrationController {
-  private final UserService service;
+  private final CreateUserUseCase createUserUseCase;
 
-  public RegistrationController(UserService service) {
-    this.service = service;
+  public RegistrationController(CreateUserUseCase createUserUseCase) {
+    this.createUserUseCase = createUserUseCase;
   }
 
   @POST
   public Response post(CreateUserCommand command) {
     try {
-      var event = service.createUser(command);
+      var event = createUserUseCase.createUser(command);
       return Response.ok(event.getId().toString()).build();
     } catch (Exception e) {
       return Response.status(Status.BAD_REQUEST).entity(e.toString()).build();
